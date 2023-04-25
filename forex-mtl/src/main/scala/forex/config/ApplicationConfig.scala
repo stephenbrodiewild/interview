b68e1fc703495a7d8d18b5253f6c5d5aed3a85a9
sbw <stephenbrodiewild@gmail.com>
2023-04-26 00:38:11 +0800

    one-frame service config handling

--- forex-mtl/src/main/scala/forex/config/ApplicationConfig.scala
+++ forex-mtl/src/main/scala/forex/config/ApplicationConfig.scala
@@ -4,10 +4,17 @@ import scala.concurrent.duration.FiniteDuration
 
 case class ApplicationConfig(
     http: HttpConfig,
+    oneFrame: OneFrameConfig 
 )
 
 case class HttpConfig(
     host: String,
     port: Int,
     timeout: FiniteDuration
+)
+
+case class OneFrameConfig(
+    host: String,
+    port: Int,
+    token: String
 )
